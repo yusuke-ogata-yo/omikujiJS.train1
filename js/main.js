51f3a76841fde7cf7c7ebf6e593a2fcd8530248f
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2019-10-23 21:49:49 +0900

    complite!

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'user strict';
+
+{
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click', () => {
+    // const n = Math.floor(Math.random() * 3);
+    // btn.textContent = n;
+
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
+    // }
+
+
+    const results = ['大吉', '中吉', '凶']
+    const rand = Math.floor(Math.random() * results.length);
+    btn.textContent = results[rand];
+  });
+}
